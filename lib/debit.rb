bd57956c1cec34b292e59329db1a5e419ded887f
Xin Wang <wangxin00163@yahoo.co.uk>
2018-01-29 12:36:31 +0000

    Created debit class which is similar to credit class

--- lib/debit.rb
+++ lib/debit.rb
@@ -1,6 +1,14 @@
 class Debit
+  attr_reader :amount, :current_balance
 
-  def initialize
+  def initialize(amount, current_balance)
+    @amount = amount
+    @current_balance = current_balance
+    @created_at = Time.now
+  end
+
+  def created_date
+    @created_at.strftime("%d/%m/%Y")
   end
 
 end
--- spec/debit_spec.rb
+++ spec/debit_spec.rb
@@ -1,1 +1,23 @@
 require 'debit'
+
+describe Debit do
+  let(:amount) {20.00}
+  let(:current_balance) {10.00}
+  subject(:debit) { described_class.new(amount, current_balance) }
+
+    it 'should have a debit' do
+      expect(subject).to be_a_kind_of(Debit)
+    end
+
+    it 'should show the date when the debit is made' do
+      expect(subject.created_date.to_s).to eq(Time.now.strftime("%d/%m/%Y").to_s)
+    end
+
+    it 'should have an amount' do
+      expect(subject.amount).to eq(20.00)
+    end
+
+    it 'should know the current_balance' do
+      expect(subject.current_balance).to eq(10.00)
+    end
+end
