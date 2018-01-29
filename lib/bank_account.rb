ea89dc743e14f1f65e3fe4ddda3aa678eca03173
Xin Wang <wangxin00163@yahoo.co.uk>
2018-01-29 11:15:39 +0000

    Passing the second test: user can deposit money in the bank

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,14 @@
 class BankAccount
   attr_reader :balance
 
-  INITIAL_BALANCE = 0
+  INITIAL_BALANCE = 0.0
 
   def initialize
     @balance = INITIAL_BALANCE
   end
 
 
+  def deposit(amount)
+    @balance += amount
+  end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -5,8 +5,13 @@ describe BankAccount do
 
   it "have an initial balance" do
     bank_account = BankAccount.new
-    expect(bank_account.balance).to eq(0)
+    expect(bank_account.balance).to eq(0.0)
   end
 
+  it "allows user to desposit money" do
+    bank_account = BankAccount.new
+    bank_account.deposit(20.00)
+    expect(bank_account.balance).to eq(20.00)
+  end
 
 end
